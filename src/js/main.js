0cfd7b75d4f2b8bd7357f492f86f0ccb1724839d
Anton Azarapin <108542363+antonazarapin@users.noreply.github.com>
2023-06-22 22:17:35 +0300

    validate and post forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import './slider';
 import modals from './modules/modals';
 import tabs from './modules/tabs';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     //div div потому что там под контентов еще 2 вложенных блока и только во втором находится контент, который под разными именами
+    forms();
 });
