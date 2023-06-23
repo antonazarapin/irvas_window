281e3cd3878c9b7c5d41a90668de9e850db73032
Anton Azarapin <108542363+antonazarapin@users.noreply.github.com>
2023-06-23 21:12:26 +0300

    calculator tabs and popups create

--- src/js/main.js
+++ src/js/main.js
@@ -8,5 +8,6 @@ window.addEventListener('DOMContentLoaded', () => {
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     //div div потому что там под контентов еще 2 вложенных блока и только во втором находится контент, который под разными именами
+    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
     forms();
 });
